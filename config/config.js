3e7b38fdd6e32c80df586ad8c846b3a09a671b75
백현영 <baeghyeon-yeong@baeghyeon-yeong-ui-MacBookPro.local>
2020-06-08 14:05:34 +0900

    Fix confing change   - confing 수정함

--- config/config.js
+++ config/config.js
@@ -2,26 +2,26 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
+    username: 'root',
     password: process.env.DB_PASSWORD,
-    host: process.env.DB_HOSTNAME,
-    database: '',
+    host: 'localhost',
+    database: 'Meet-you',
     dialect: 'mysql',
     operatorsAliases: false, // 연산자에 대한 별칭을 사용할 것인지? 뭔 말인지..
   },
   // test: {
   //   username: 'root',
-  //   password: null,
+  //   password: process.env.DB_PASSWORD,
   //   database: 'database_test',
   //   host: '127.0.0.1',
   //   dialect: 'mysql',
   //   operatorsAliases: false,
   // },
   production: {
-    username: process.env.RDS_USERNAME,
+    username: 'lostJK',
     password: process.env.RDS_PASSWORD,
-    host: process.env.RDS_HOSTNAME,
-    database: '',
+    host: '',
+    database: 'Meet-you',
     dialect: 'mysql',
     operatorsAliases: false,
   },
