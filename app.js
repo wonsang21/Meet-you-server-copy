1436fd648896c360ba691ac7233ccb5ede126b58
kthdd1234 <kthdd12@daum.net>
2020-06-29 17:47:36 +0900

    commpleted chatting server logic

--- app.js
+++ app.js
@@ -12,14 +12,9 @@ const pointRouter = require('./routes/point');
 const { chattings } = require('./models');
 const http = require('http');
 const server = http.createServer(app);
-const io = require('socket.io')(server, {
-  log: false,
-  origins: '*:*',
-  pingInterval: 3000,
-  pingTimeout: 5000,
-});
+const io = require('socket.io')(server);
 const { rooms } = require('./rooms');
-
+const { Op } = require('sequelize');
 app.get('/', (req, res) => {
   res.send('Hello Express');
 });
@@ -50,7 +45,6 @@ app.use(
   })
 );
 // socket io client 통신부분
-
 // socket 통신 연결 성공시
 io.on('connection', async (socket) => {
   let roomNames = await rooms();
@@ -76,81 +70,63 @@ io.on('connection', async (socket) => {
         ////////////////////////////////////////////
         ////DB에서 룸에 저장된 메시지를 불러오는 코드작성/////
         ////////////////////////////////////////////
-        let username = roomInfo.myUsername;
         let roomname = room;
         chattings
-          .findOne({
+          .findAll({
             where: {
-              userName: username,
               roomName: roomname,
             },
             attributes: ['message'],
             raw: true,
           })
           .then((data) => {
-            let data_parse = JSON.parse(data.message);
-            console.log('DB에서 룸에 저장된 메시지를 불러옴 => ', data_parse);
-            socket.emit('messages', data_parse); // DB에서 불러온 채팅방 메시지를 클라이언트로 보내준다.
+            if (data[0].message !== null) {
+              let data_parse = JSON.parse(data[0].message);
+              console.log('DB에서 룸에 저장된 메시지를 불러옴 => ', data_parse);
+              socket.emit('messages', data_parse); // DB에서 불러온 채팅방 메시지를 클라이언트로 보내준다.
+            }
           });
         break;
       } else {
         count++;
+        console.log('카운트 값: ', count);
         // 특정 룸이 없다면 새로 룸만 생성한다.
         if (count === roomNames.length) {
           console.log('fail', '해당룸은 존재 하지 않아서 새로 생성하겠습니다.');
           socket.join(roomInfo.myUsername + roomInfo.username);
           const createRoomName = roomInfo.myUsername + roomInfo.username;
-          const name_Arr = [roomInfo.myUsername, roomInfo.username];
-          name_Arr.forEach((name) => {
-            chattings.create({
-              userName: name,
-              roomName: createRoomName,
-            });
+          chattings.create({
+            userName: roomInfo.myUsername,
+            roomName: createRoomName,
           });
           console.log(
             '현재 소켓이 접속되어 있는 룸번호: ',
             roomInfo.myUsername + roomInfo.username
           );
+          count = 0;
           break;
         }
       }
     }
-    socket.on('message', (message) => {
+    socket.on('message', (newMessage) => {
       const myUsername = roomInfo.myUsername;
       const pairname = roomInfo.username;
-      const roomName = myUsername + pairname;
-
-      socket.broadcast.to(roomName).emit('message', message); // {...}
-      console.log('상대방에게 보내는 메시지', message);
-
+      const roomName1 = myUsername + pairname;
+      const roomName2 = pairname + myUsername;
       chattings
-        .findOne({
+        .findAll({
           where: {
-            userName: myUsername,
-            roomName: roomName,
+            [Op.or]: [{ roomName: roomName1 }, { roomName: roomName2 }],
           },
-          attributes: ['message'],
+          attributes: ['message', 'roomName'],
+          raw: true,
         })
-        .then((DB_message) => {
-          let data = DB_message.get({ plain: true });
-          if (data.message === null) {
-            let array = [];
-            array.push(DB_message);
-            let json = JSON.stringify(array);
-            chattings.update(
-              {
-                message: json,
-              },
-              {
-                where: {
-                  userName: myUsername,
-                  roomName: roomName,
-                },
-              }
-            );
-          } else {
-            let message_parse = JSON.parse(data.message);
-            message_parse.unshift(message);
+        .then((data) => {
+          console.log('DB: ', data);
+          if (data[0].message !== null) {
+            let message = data[0].message;
+            let message_parse = JSON.parse(message);
+            message_parse.unshift(newMessage);
             let json = JSON.stringify(message_parse);
             chattings.update(
               {
@@ -158,18 +134,32 @@ io.on('connection', async (socket) => {
               },
               {
                 where: {
-                  userName: myUsername,
-                  roomName: roomName,
+                  roomName: data[0].roomName,
+                },
+              }
+            );
+          } else {
+            let array = [];
+            array.push(newMessage);
+            let json = JSON.stringify(array);
+            chattings.update(
+              {
+                message: json,
+              },
+              {
+                where: {
+                  roomName: data[0].roomName,
                 },
               }
             );
           }
+          socket.broadcast.to(data[0].roomName).emit('message', newMessage); // {...}
+          console.log('상대방에게 보내는 메시지', newMessage);
         });
     });
   });
   // 클라이언트에서 보내온 채팅메시지를 같은룸의 상대방에게 보낸다.
 });
-
 // roomInfo.myUsername = '공유',
 // message = {...}
 // const myUsername = '공유';
@@ -178,10 +168,8 @@ io.on('connection', async (socket) => {
 // const user_message = {
 //   공유왈: '김지원 예쁘다',
 // };
-
 app.post('/roomName', (req, res) => {
   const { username } = req.body;
-
   chattings
     .findAll({
       where: {
